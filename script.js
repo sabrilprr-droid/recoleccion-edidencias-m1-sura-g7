d5a7c512b3786a5e59cdaace4bc77e7a55a1ec53
Samuel abril <sabrilprr@gmail.com>
2025-11-07 20:48:40 -0500

    se realiza el case 1 validado en consola, y se limita el while a la opcion 5 para terminar el ciclo

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ let altura = NaN;     // Almacena la altura del usuario
 let bmi = NaN;        // Almacena el indice de masa corporal
 let opcion = "";      // Controla la opcion seleccionada
 
-while {
+while (opcion !== "5") {
 console.log("=== MENU VIDA SANA ===\n1. Ingresar Peso\n2. Ingresar Altura\n3. Calcular BMI\n4. Interpretar Resultado\n5. Salir");
 
   opcion = prompt("Selecciona una opcion de 1 a 5: \n=== MENU VIDA SANA ===\n1. Ingresar Peso\n2. Ingresar Altura\n3. Calcular BMI\n4. Interpretar Resultado\n5. Salir ");
 
- switch {
+ switch (opcion) {
     case "1":
-      
+      let entradaTextoPesoIngresadoPorUsuarioEnKilogramos = prompt("Ingresa tu peso en Kg:");
+  let valorNumericoPesoIngresadoPorUsuarioEnKilogramos = Number(entradaTextoPesoIngresadoPorUsuarioEnKilogramos);
+
+  if (!isNaN(valorNumericoPesoIngresadoPorUsuarioEnKilogramos)) {
+    if (valorNumericoPesoIngresadoPorUsuarioEnKilogramos > 0 && valorNumericoPesoIngresadoPorUsuarioEnKilogramos < 500) {
+      peso = valorNumericoPesoIngresadoPorUsuarioEnKilogramos;
+      console.log("Peso guardado correctamente.");
+    } else {
+      console.log("Dato inválido. El peso debe ser mayor a 0 y menor a 500.");
+    }
+  } else {
+    console.log("Entrada inválida. Debes ingresar un número.");
+  }
       break;
 
     case "2":

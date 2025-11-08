225bfb28e8dee6ec13ceaf3ec349804de6442b18
Samuel abril <sabrilprr@gmail.com>
2025-11-07 19:42:15 -0500

    Inicializo variables para peso, altura, BMI y opción del menú

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let peso = NaN;       // Almacena el peso del usuario
+let altura = NaN;     // Almacena la altura del usuario
+let bmi = NaN;        // Almacena el indice de masa corporal
+let opcion = "";      // Controla la opcion seleccionada
 
+while 
+console.log("=== MENU VIDA SANA ===\n1. Ingresar Peso\n2. Ingresar Altura\n3. Calcular BMI\n4. Interpretar Resultado\n5. Salir");
+
+  opcion = prompt("Selecciona una opcion de 1 a 5: \n=== MENU VIDA SANA ===\n1. Ingresar Peso\n2. Ingresar Altura\n3. Calcular BMI\n4. Interpretar Resultado\n5. Salir ");
+
+ switch 
+    case "1":
+      
+      break;
+
+    case "2":
+      
+      break;
+
+    case "3":
+      
+      break;
+
+    case "4":
+     
+      break;
+
+    case "5":
+      console.log("Gracias por usar VidaSana.");
+      break;
+
+    default:
+      console.log("Opción no válida. Intenta de nuevo.");
+  }
+}

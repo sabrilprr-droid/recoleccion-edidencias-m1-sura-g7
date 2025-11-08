8322cb18b72cd214a00c70a47f9cce2e7e693a84
Samuel abril <sabrilprr@gmail.com>
2025-11-07 21:15:42 -0500

    se realiza el case 3 con las validaciones en consola, el imc y la clasificacion por peso

--- script.js
+++ script.js
@@ -43,7 +43,25 @@ console.log("=== MENU VIDA SANA ===\n1. Ingresar Peso\n2. Ingresar Altura\n3. Ca
       break;
 
     case "3":
-      
+      if (!isNaN(peso) && !isNaN(altura)) {
+    let resultadoIndiceMasaCorporalCalculadoConPesoYAltura = peso / (altura * altura); //formula para hallar el imc
+    bmi = resultadoIndiceMasaCorporalCalculadoConPesoYAltura;
+
+    let bmiRedondeadoDosDecimales = Math.round(bmi * 100) / 100; // el math.round se utilizo para que el resultado de la operacion no diera tanto decimal
+    console.log("Tu índice de masa corporal (IMC) es: " + bmiRedondeadoDosDecimales);
+
+    if (bmi < 18.5) {
+      console.log("Clasificación: Bajo peso");
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      console.log("Clasificación: Peso normal");
+    } else if (bmi >= 25 && bmi < 29.9) {
+      console.log("Clasificación: Sobrepeso");
+    } else {
+      console.log("Clasificación: Obesidad");
+    }
+  } else {
+    console.log("No se puede calcular el IMC. Asegúrate de haber ingresado peso y altura válidos.");
+  }
       break;
 
     case "4":
